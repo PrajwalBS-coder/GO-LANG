042c95f00cf3cfa1ce496cd75577b6c83008252b
Prajwal B S <legendamin008@gmail.com>
2023-02-03 15:09:16 +0530

    success

--- sec.go
+++ sec.go
@@ -9,43 +9,8 @@ fmt.Print("Enter No")
 fmt.Scan(&k)
 fmt.Print("Enter No 2")	
 fmt.Scan(&j)
-fmt.Print("Addition = ",k+j)
-
-
-
+fmt.Println("Addition = ",k+j)
+fmt.Println("Substraction=",k-j)
+fmt.Println("Mulpliaction=",k*j)
+fmt.Println("Devision=",k/j)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
