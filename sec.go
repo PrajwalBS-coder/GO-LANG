44df78f61e610b65c3aaad1ba6f96a86fb9ffc64
Prajwal B S <legendamin008@gmail.com>
2023-02-03 15:01:39 +0530

    commit -m "succes"

---
+++ sec.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func main(){
+var k,j int12
+
+fmt.Print("Enter No")	
+fmt.Scan(&k)
+fmt.Print("Enter No 2")	
+fmt.Scan(&j)
+fmt.Print("Addition = ",k+j)
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
